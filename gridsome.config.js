ab4c24d25ad14d2089d8ea2bb604c99d8e111e0f
wilde <luke.wilde@gmail.com>
2022-06-01 16:46:59 +0100

    Correcting title.

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,6 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Luke Wilde - JavaScript Engineer',
+  siteName: 'Luke Wilde - Software Engineer',
   plugins: []
 }
